2e127853e678bfc08dabb46ff56c52cb56125d4a
kyhe <kyhe@uchicago.edu>
2019-02-13 21:16:49 -0600

    added more questions

--- questions/global.js
+++ questions/global.js
@@ -1,12 +1,24 @@
 function rightAnswer() {
-    
+    var sound = new Audio('correct.wav');
+    sound.volume = 0.02;
+    sound.play();
 }
 
 function wrongAnswer() {
     var decodedCookie = decodeURIComponent(document.cookie);
     var old_value = decodedCookie.split('=')[1];
     var sound = new Audio('wrong_answer.mp3');
-    sound.volume = 0.01;
+    sound.volume = 0.02;
     sound.play();
     document.cookie = "num_wrongs=" + (old_value + 1);
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function next_question(question_num) {
+    rightAnswer();
+    await sleep(1000);
+    location.href = "./q" + question_num + ".html";
 }
