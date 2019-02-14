6e0dcf49ba7cee8cc16cc6ec1beed1d6cd24d89f
kevin <kyhe@uchicago.edu>
2019-02-14 00:34:09 -0600

    added up to q11

--- questions/global.js
+++ questions/global.js
@@ -4,12 +4,16 @@ function rightAnswer() {
     sound.play();
 }
 
-function wrongAnswer() {
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var old_value = decodedCookie.split('=')[1];
+function playWrongAnswer() {
     var sound = new Audio('wrong_answer.mp3');
     sound.volume = 0.02;
     sound.play();
+}
+
+function wrongAnswer() {
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var old_value = decodedCookie.split('=')[1];
+    playWrongAnswer();
     document.cookie = "num_wrongs=" + (old_value + 1);
 }
 
